32df87593ef62cd121cced42e6e2059c64c9be51
what <906885696@qq.com>
2022-09-11 20:24:46 +0800

    initialize project and vscode configuration

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using std::endl;
+using std::cout;
+
+int main()
+{
+    cout << "hello, world." << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "i = ++ " << i << endl;
+    }
+    return 0;
+}
